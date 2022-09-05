774afce52d46bec94fad17775b7671ce062681cb
hasindu200 <hasindukl@gmail.com>
2022-09-05 22:04:25 +0530

    initialize app js file

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const customer = require('./routes/customer.js')
+const item = require('./routes/item')
+const order = require('./routes/order')
+const orderDetails = require('./routes/orderDetails')
+
+const app = express();
+const port = 4000;
+
+app.use(express.json());
+app.use('/customer',customer)
+app.use('/item',item)
+app.use('/order',order)
+app.use('/orderDetails',orderDetails)
+
